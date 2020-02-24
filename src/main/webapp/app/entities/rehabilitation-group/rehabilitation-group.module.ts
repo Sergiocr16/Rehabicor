7abe4e8fb9c583cc9bf716e8438c8af7d847adab
Sergio Castro <sergiojcr16@gmail.com>
2020-02-24 15:26:00 -0600

    Se soluciona problema con dependencias

--- src/main/webapp/app/entities/rehabilitation-group/rehabilitation-group.module.ts
+++ src/main/webapp/app/entities/rehabilitation-group/rehabilitation-group.module.ts
@@ -14,17 +14,22 @@ import {
     rehabilitationGroupPopupRoute
 } from './';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RehabilitationGroupPanelComponent } from 'app/entities/rehabilitation-group/rehabilitation-group-panel.component';
+import { RehabilitationGroupClinicalCharacteristicsComponent } from 'app/entities/rehabilitation-group/rehabilitation-group-clinical-characteristics.component';
+import { ChartsModule } from 'ng2-charts';
 
 const ENTITY_STATES = [...rehabilitationGroupRoute, ...rehabilitationGroupPopupRoute];
 
 @NgModule({
-    imports: [RehabicorSharedModule, RouterModule.forChild(ENTITY_STATES), ReactiveFormsModule],
+    imports: [RehabicorSharedModule, RouterModule.forChild(ENTITY_STATES), ReactiveFormsModule, ChartsModule],
     declarations: [
         RehabilitationGroupComponent,
         RehabilitationGroupDetailComponent,
         RehabilitationGroupUpdateComponent,
         RehabilitationGroupDeleteDialogComponent,
-        RehabilitationGroupDeletePopupComponent
+        RehabilitationGroupDeletePopupComponent,
+        RehabilitationGroupPanelComponent,
+        RehabilitationGroupClinicalCharacteristicsComponent
     ],
     entryComponents: [
         RehabilitationGroupComponent,
